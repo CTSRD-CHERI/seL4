9e301c21f391c5adda3ace4fc0df9285b5b92198
Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>
2025-02-20 15:27:10 +0000

    [cheri][riscv] Add a new hobgoblin platform
    This commit adds support for Codasip's hobgoblin platform which includes the
    x730 processor [1]. The processor implements CHERI-RISC-V standard and should
    be compliant with the CHERI-RISC-V draft standard [2].
    
    This port has been tested and validated on both QEMU's hobgoblin-vcu118 [3]
    machine as well as the VCU-118 FPGA board passing sel4tests, with the following
    variants:
    1- Vanilla RISC-V (non-CHERI).
    2- Hybrid kernel, purecap userspace (sel4test)
    3- Purecap kernel and userspace (sel4test)
    
    [1] https://codasip.com/solutions/riscv-processor-safety-security/cheri/x730-risc-v-application-processor/
    [2] https://github.com/riscv/riscv-cheri/releases/tag/v0.9.4-release
    [3] https://github.com/CHERI-Alliance/qemu/tree/codasip-cheri-riscv_v3
    
    Signed-off-by: Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>

---
+++ src/plat/hobgoblin-vcu118/config.cmake
@@ -0,0 +1,38 @@
+#
+# Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+# Copyright (c) 2025, Capabilities Ltd <heshamalmatary@capabilitieslimited.co.uk>
+
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+declare_platform(hobgoblin-vcu118 KernelPlatformHobGoblinVcu118 PLAT_HOBGOBLIN_VCU118 KernelSel4ArchRiscV64)
+
+if(KernelPlatformHobGoblinVcu118)
+    declare_seL4_arch(riscv64)
+    config_set(KernelRiscVPlatform RISCV_PLAT "hobgoblin-vcu118")
+    config_set(KernelOpenSBIPlatform OPENSBI_PLATFORM "generic")
+    config_set(KernelPlatformFirstHartID FIRST_HART_ID 0)
+
+    # CHERI related configs
+    if(HaveCheri)
+        set(KernelArchCheriRiscv ON)
+        set(KernelArchCheriRiscvV09 ON)
+
+        set(QEMU_ARCH "${KernelSel4Arch}cheri")
+        message(STATUS "Buidling with CHERI support")
+    else()
+        set(QEMU_ARCH "${KernelSel4Arch}")
+    endif()
+
+    list(APPEND KernelDTSList "tools/dts/hobgoblin-vcu118.dts")
+    list(APPEND KernelDTSList "src/plat/hobgoblin-vcu118/overlay-hobgoblin-vcu118.dts")
+    declare_default_headers(
+        TIMER_FREQUENCY 0x5f5e100
+        MAX_IRQ 30
+        INTERRUPT_CONTROLLER drivers/irq/riscv_plic0.h
+    )
+else()
+    unset(KernelPlatformFirstHartID CACHE)
+endif()
